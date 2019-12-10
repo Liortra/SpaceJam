0a2453bf71d0c397009d8f1cb11a8fabcf63e33b
AvrahamRada <avraham1992rada@gmail.com>
2019-12-10 15:12:56 +0200

    Updating LogIn screen

--- app/src/main/java/com/example/spacejam/Login.java
+++ app/src/main/java/com/example/spacejam/Login.java
@@ -3,18 +3,24 @@ package com.example.spacejam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class Login extends AppCompatActivity {
 
     private EditText nameOfPlayer;
     private Button play;
     private Button exit;
+    private ImageView volume;
+    private  boolean volumeMute = false;
+    private MediaPlayer loginSong;
 
     private final String bundleString = "player_name";
     Bundle bundle; // Used like a Pipe to transfer values between windows
@@ -24,11 +30,16 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         getSupportActionBar().hide(); // Disappearing of the main bar
+        // Play Music
+        loginSong = MediaPlayer.create(getApplicationContext(), R.raw.gameon);
+        loginSong.setLooping(true);
+        loginSong.start();
 
-        nameOfPlayer = (EditText) findViewById(R.id.editText);
+        nameOfPlayer = (EditText) findViewById(R.id.nameOfPlayer);
         play = (Button) findViewById(R.id.btn_startGame);
         exit = (Button) findViewById(R.id.exit);
         bundle = new Bundle();
+        volume = (ImageView) findViewById(R.id.volume);
 
         nameOfPlayer.addTextChangedListener(watcher);
     }
@@ -65,5 +76,19 @@ public class Login extends AppCompatActivity {
     public void clickToExit(View v) {
         moveTaskToBack(true);
         System.exit(0);
+    }
+
+    // Set volume ON/OFF
+    public void changeVolume(View view) {
+        if (!volumeMute){
+            volumeMute = true;
+            view.setBackgroundResource(R.drawable.volume_off);
+            loginSong.pause();
+        }
+        else{
+            volumeMute = false;
+            view.setBackgroundResource(R.drawable.volume_on);
+            loginSong.start();
+        }
     }
 }
